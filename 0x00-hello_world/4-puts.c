90cff542b7be53848fe6894c002d4869c3e9e2e3
sebakazz <chizondaseba@gmail.com>
2023-05-18 10:30:39 -0700

    This is the 4-puts.c commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+nclude<stdio.h>
+int main(void)
+/**
+ * main - This program print some specific text about programming
+ * Return: 0 (Success)
+ * */
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
